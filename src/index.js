29534196cb41273bce3a8bf63147891dd55736db
LanthDeveloper <lanthdev@gmail.com>
2024-12-27 18:20:45 -0500

    test

--- src/index.js
+++
@@ -1,9 +0,0 @@
-//src/index.js
-
-import app from "./app.js";
-import { SERVER_PORT, SERVER_HOST } from "./vars/vServer.js";
-
-// Start the server
-app.listen(SERVER_PORT, () => {
-  console.log(`✅ Backend running on: http://${SERVER_HOST}:${SERVER_PORT}`);
-});
